dd5a8c25447f2d837325c781a12dff7c637bcb2b
Afthonia <aisesen765@gmail.com>
2024-04-29 18:33:54 +0300

    models are done

---
+++ lib/models/quote_model.dart
@@ -0,0 +1,38 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class QuoteModel {
+  int id;
+  int userID;
+  String content;
+  DateTime createdAt;
+  
+  QuoteModel({
+    required this.id,
+    required this.userID,
+    required this.content,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'user_id': userID,
+      'content': content,
+      'created_at': createdAt.millisecondsSinceEpoch,
+    };
+  }
+
+  factory QuoteModel.fromMap(Map<String, dynamic> map) {
+    return QuoteModel(
+      id: map['id'] as int,
+      userID: map['userID'] as int,
+      content: map['content'] as String,
+      createdAt: DateTime.fromMillisecondsSinceEpoch(map['created_at'] as int),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory QuoteModel.fromJson(String source) => QuoteModel.fromMap(json.decode(source) as Map<String, dynamic>);
+}
--- lib/utils/globals.dart
+++ lib/utils/globals.dart
@@ -1,8 +1,9 @@
 class Globals {
   static int bookID = 1;
-  static int characterID = 1;  
+  static int characterID = 1;
   static int chapterID = 1;
   static int speciesID = 1;
   static int itemID = 1;
   static int noteID = 1;
+  static int quoteID = 1;
 }
